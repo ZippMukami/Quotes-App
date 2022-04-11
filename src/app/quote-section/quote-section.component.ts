0fdc27f4d2d836f9104997a205d8cb0a1b81146e
ZippMukami <zimukami@gmail.com>
2022-04-11 23:58:19 +0300

    adds quote detail component.

--- src/app/quote-section/quote-section.component.ts
+++ src/app/quote-section/quote-section.component.ts
@@ -6,12 +6,12 @@ import { Quotes } from '../quotes';
   styleUrls: ['./quote-section.component.css']
 })
 export class QuoteSectionComponent implements OnInit {
-  quotes:Quotes[]=[
-    {id:1,quote:'Fake it till you make it!',author:'Budha', user:'Gama', date:new Date, upvote:0, downvote:0},
-    {id:2,quote:'Keep moving!',author:'Budha', user:'Gama', date:new Date, upvote:0, downvote:0},
-    {id:3,quote:'The purpose of our lives is to be happy!',author:'Dalai Lama', user:'Gama', date:new Date, upvote:0, downvote:0},
-    {id:4,quote:'Life is what happens when youre busy making other plans!',author:'Stephen King', user:'Gama', date:new Date, upvote:0, downvote:0},
-    {id:5,quote:'You only live once, but if you do it right, once is enough!',author:'Mae West', user:'Gama', date:new Date, upvote:0, downvote:0}
+  quotes: Quotes[] = [
+    { id: 1, quote: 'Fake it till you make it!', author: 'Budha', user: 'Gama', date: new Date, upvote: 0, downvote: 0 },
+    { id: 2, quote: 'Keep moving!', author: 'Budha', user: 'Gama', date: new Date, upvote: 0, downvote: 0 },
+    { id: 3, quote: 'The purpose of our lives is to be happy!', author: 'Dalai Lama', user: 'Gama', date: new Date, upvote: 0, downvote: 0 },
+    { id: 4, quote: 'Life is what happens when youre busy making other plans!', author: 'Stephen King', user: 'Gama', date: new Date, upvote: 0, downvote: 0 },
+    { id: 5, quote: 'You only live once, but if you do it right, once is enough!', author: 'Mae West', user: 'Gama', date: new Date, upvote: 0, downvote: 0 }
 
   ];
   constructor() { }
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,10 +1,10 @@
 export class Quotes {
-    id!:number;
-    quote!:string;
-    author!:string;
-    user!:string;
-    date!:Date;
-    upvote!:number;
-    downvote!:number;
-    
+    id!: number;
+    quote!: string;
+    author!: string;
+    user!: string;
+    date!: Date;
+    upvote!: number;
+    downvote!: number;
+
 }
