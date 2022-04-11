6cc4406c766da7238c88500d0408ef40a04ca81e
ZippMukami <zimukami@gmail.com>
2022-04-12 00:27:27 +0300

    fixes bugs.

--- src/app/quote-section/quote-section.component.ts
+++ src/app/quote-section/quote-section.component.ts
@@ -7,12 +7,11 @@ import { Quotes } from '../quotes';
 })
 export class QuoteSectionComponent implements OnInit {
   quotes: Quotes[] = [
-    { id: 1, quote: 'Fake it till you make it!', author: 'Budha', user: 'Gama', date: new Date, upvote: 0, downvote: 0 },
-    { id: 2, quote: 'Keep moving!', author: 'Budha', user: 'Gama', date: new Date, upvote: 0, downvote: 0 },
-    { id: 3, quote: 'The purpose of our lives is to be happy!', author: 'Dalai Lama', user: 'Gama', date: new Date, upvote: 0, downvote: 0 },
-    { id: 4, quote: 'Life is what happens when youre busy making other plans!', author: 'Stephen King', user: 'Gama', date: new Date, upvote: 0, downvote: 0 },
-    { id: 5, quote: 'You only live once, but if you do it right, once is enough!', author: 'Mae West', user: 'Gama', date: new Date, upvote: 0, downvote: 0 }
-
+    new Quotes(  1, 'Fake it till you make it!', 'Budha', 'Gama', new Date, 0, 0),
+    new Quotes(  2, 'Keep moving!', 'Budha', 'Gama', new Date, 0, 0 ),
+    new Quotes(  3, 'The purpose of our lives is to be happy!', 'Dalai Lama', 'Gama', new Date, 0, 0 ),
+    new Quotes(  4, 'Life is what happens when youre busy making other plans!', 'Stephen King', 'Gama', new Date, 0, 0 ),
+    new Quotes(  5, 'You only live once, but if you do it right, once is enough!', 'Mae West', 'Gama', new Date, 0,0)
   ];
   constructor() { }
 
