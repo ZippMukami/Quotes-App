6c62e26450dc05b389977f47f9d8850fbc193b4f
ZippMukami <zimukami@gmail.com>
2022-04-12 01:15:08 +0300

    adds an emitter.

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quotes } from '../quotes';
 
 @Component({
@@ -8,6 +8,9 @@ import { Quotes } from '../quotes';
 })
 export class QuoteDetailsComponent implements OnInit {
   @Input() quote!: Quotes;
+  @Output() done = new EventEmitter <boolean>();
+quoteDone(complete:boolean){this.done.emit(complete)};
+
   constructor() { }
 
   ngOnInit(): void {
