589a03d3cfbd2602ddabee9f7b0616357d75c746
ZippMukami <zimukami@gmail.com>
2022-04-11 23:28:23 +0300

    adds class quotes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Quotes } from './quotes';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes-IP';
+quotes:Quotes[]=[
+  {id:1,quote:'Fake it till you make it!',author:'Budha', user:'Gama', date:new Date, upvote:0, downvote:0}
+];
+
+
+
 }
---
+++ src/app/quotes.ts
@@ -0,0 +1,10 @@
+export class Quotes {
+    id!:number;
+    quote!:string;
+    author!:string;
+    user!:string;
+    date!:Date;
+    upvote!:number;
+    downvote!:number;
+    
+}
