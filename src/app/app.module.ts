3f241a3fb223b252a13f4c65966aff33888aeeb5
ZippMukami <zimukami@gmail.com>
2022-04-11 23:45:50 +0300

    adds component quote details.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteSectionComponent } from './quote-section/quote-section.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteSectionComponent
+    QuoteSectionComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
