282e91c6817fd517c1f7d773c0154eefc6226972
ZippMukami <zimukami@gmail.com>
2022-04-12 01:48:25 +0300

    adds a toggle and untoggle function.

--- src/app/quote-section/quote-section.component.ts
+++ src/app/quote-section/quote-section.component.ts
@@ -7,17 +7,20 @@ import { Quotes } from '../quotes';
 })
 export class QuoteSectionComponent implements OnInit {
   quotes: Quotes[] = [
-    new Quotes(  1, 'Fake it till you make it!', 'Budha', 'Gama', new Date, 0, 0),
-    new Quotes(  2, 'Keep moving!', 'Budha', 'Gama', new Date, 0, 0 ),
-    new Quotes(  3, 'The purpose of our lives is to be happy!', 'Dalai Lama', 'Gama', new Date, 0, 0 ),
-    new Quotes(  4, 'Life is what happens when youre busy making other plans!', 'Stephen King', 'Gama', new Date, 0, 0 ),
-    new Quotes(  5, 'You only live once, but if you do it right, once is enough!', 'Mae West', 'Gama', new Date, 0,0)
+    new Quotes(1, 'Fake it till you make it!', 'Budha', 'Gama', new Date, 0, 0),
+    new Quotes(2, 'Keep moving!', 'Budha', 'Gama', new Date, 0, 0),
+    new Quotes(3, 'The purpose of our lives is to be happy!', 'Dalai Lama', 'Gama', new Date, 0, 0),
+    new Quotes(4, 'Life is what happens when youre busy making other plans!', 'Stephen King', 'Gama', new Date, 0, 0),
+    new Quotes(5, 'You only live once, but if you do it right, once is enough!', 'Mae West', 'Gama', new Date, 0, 0)
   ];
 
-  toggleDetails(index){
-    this.quotes[index].showDescription= !this.quotes[index].showDescription;
+  toggleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
   };
 
+  completeQuote(done, index) {
+    this.quotes.splice(index, 1);
+  };
   constructor() { }
 
   ngOnInit(): void {
